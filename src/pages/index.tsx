01b22775562780208e3719298c5eae05fb60ef88
ffatique <f_fatique@hotmail.com>
2023-05-12 13:00:18 -0300

    new edit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,6 @@
 import Head from 'next/head'
 import styles from '../styles/home.module.scss'
 import Image from 'next/image'
-import foto from '../../public/images/foto.png'
-import imagem1 from '../../public/images/imagem1.png'
-import imagem2 from '../../public/images/imagem2.png'
-import imagem3 from '../../public/images/imagem3.png'
-import imagem4 from '../../public/images/imagem4.png'
-import imagem5 from '../../public/images/imagem5.png'
 import ens from '../../public/images/ens.png'
 import logo from '../../public/images/logo-black.png'
 import lens from '../../public/images/lens.png'
@@ -18,12 +12,42 @@ import { RWebShare } from "react-web-share"
 import { VscGithub } from 'react-icons/vsc'
 
 
-export default function Home(){
+interface Props {
+  bname: string,
+  btitle: string,
+  bens: string,
+  blinkOne: string,
+  blinkTwo: string,
+  blinkThree: string,
+  blinkFour: string,
+  blinkFive: string,
+  btitleOne: string,
+  btitleTwo: string,
+  btitleThree: string,
+  btitleFour: string,
+  btitleFive: string,
+  blens: string,
+  btwitter: string,
+  byoutube: string,
+  binstagram: string,
+  blinkedin: string,
+  bgithub: string,
+  bemail: string,
+  bavatarUrl: string,
+  bavatarOne: string,
+  bavatarTwo: string,
+  bavatarThree: string,
+  bavatarFour: string,
+  bavatarFive: string
+}
+
+export default function Home({ bname, btitle, bens, blinkOne, blinkTwo, blinkThree, blinkFour, blinkFive, btitleOne, btitleTwo, btitleThree, btitleFour,
+  btitleFive, blens, btwitter, byoutube, binstagram, blinkedin, bgithub, bemail, bavatarUrl, bavatarOne, bavatarTwo, bavatarThree, bavatarFour, bavatarFive}: Props){
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>Linkeh | Felipe Fatique</title>
+        <title>Linkeh | {bname}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta content="#515AFF" name="theme-color"></meta>
       </Head>
@@ -32,9 +56,9 @@ export default function Home(){
         
         <RWebShare
           data={{
-            text: "Link | Felipe Fatique",
+            text: `Linkeh | ${bname}`,
             url: "",
-            title: "Link | Felipe Fatique",
+            title: `Linkeh | ${bname}`,
           }}
           onClick={() => console.log("Shared Successfully!")}
         >
@@ -49,86 +73,144 @@ export default function Home(){
 
       <main className={styles.mainContainer}>
 
-        <Image src={foto} alt="Foto Vinícius Bedum" />
+        <Image src={bavatarUrl} width={120} height={120} alt={`Foto ${bname}`} />
 
-        <h2>Felipe Fatique</h2>
-        <h3>Desenvolvedor FullStack JS, Solidity & DApps.</h3>
+        <h2>{bname}</h2>
+        <h3>{btitle}</h3>
         <div className={styles.ensContainer}>
           <Image src={ens} alt="Logo ENS" />
-          <p>fatique.eth</p>
+          <p>{bens}</p>
         </div>
         
-        <h4></h4>
+        {blinkOne == null?
+          <></>
+        : 
+          <div className={styles.buttons}>
+            <Link target="_blank" href={blinkOne}>
+              <button><Image src={bavatarOne} width={40} height={40} alt={'Logo Linkeh One'}/>
+                <p>{btitleOne}</p>
+              </button>
+            </Link>
+          </div>
+        }
         
-        <div className={styles.buttons}>
-          <Link target="_blank" href="https://www.pfpstudio.xyz/">
-            <button><Image src={imagem1} alt="link imagem "/>
-              <p>PFP Studio</p>
-            </button>
-          </Link>
-        </div>
+        {blinkTwo == null?
+          <></>
+        : 
+          <div className={styles.buttons}>
+            <Link target="_blank" href={blinkTwo}>
+              <button><Image src={bavatarTwo} width={40} height={40} alt={'Logo Linkeh Two'}/>
+                <p>{btitleTwo}</p>
+              </button>
+            </Link>
+          </div>
+        }
 
-        <div className={styles.buttons}>
-           <Link target="_blank" href="https://www.xcambo.me/">
-            <button><Image src={imagem2} alt="link imagem 3"/>
-              <p>XCambo</p>
-            </button>
-          </Link>
-        </div>
+        {blinkThree == null?
+          <></>
+        : 
+          <div className={styles.buttons}>
+            <Link target="_blank" href={blinkThree}>
+              <button><Image src={bavatarThree} width={40} height={40} alt={'Logo Linkeh Three'}/>
+                <p>{btitleThree}</p>
+              </button>
+            </Link>
+          </div>
+        }
 
+        {blinkFour== null?
+          <></>
+        : 
+          <div className={styles.buttons}>
+            <Link target="_blank" href={blinkFour}>
+              <button><Image src={bavatarFour} width={40} height={40} alt={'Logo Linkeh Four'}/>
+                <p>{btitleFour}</p>
+              </button>
+            </Link>
+          </div>
+       }
 
-        <div className={styles.buttons}>
-          <Link target="_blank" href="https://discord.com/invite/2jhsuJvqPd">
-            <button><Image src={imagem3} alt="link imagem 4"/>
-              <p>Bootcamp by IBEED Studio & Sozei</p>
-            </button>
-          </Link>
-        </div>
-
-        <p>SOZEI</p>
-
-        <div className={styles.buttons}>
-          <Link target="_blank" href="https://sozei.com.br/">
-            <button><Image src={imagem4} alt="link imagem 5" />
-              <p>Traga sua empresa para a Web3</p>
-            </button>
-          </Link>
-        </div>
-
-        <div className={styles.buttons}>
-          <Link target="_blank" href="https://calendly.com/felipe-fatique/web3-free-consulting">
-            <button><Image src={imagem5} alt="link imagem 5" />
-              <p>Free Consulting Web3</p>
-            </button>
-          </Link>
-        </div>
+        {blinkFive == null?
+          <></>
+        : 
+          <div className={styles.buttons}>
+            <Link target="_blank" href={blinkFive}>
+              <button><Image src={bavatarFive} width={40} height={40} alt={'Logo Linkeh Five'}/>
+                <p>{btitleFive}</p>
+              </button>
+            </Link>
+          </div>
+        }
 
       </main>
 
       <div className={styles.footerContainer}>
-        <Link target="_blank" href="https://twitter.com/f_fatique">
-          <CiTwitter size={35} color="black" />
-        </Link>
-        <Link target="_blank" href="https://www.instagram.com/ffatique/">
-          <CiInstagram size={32} color="black" />
-        </Link>
-        <Link target="_blank" href="https://www.linkedin.com/in/felipe-fatique/">
-          <CiLinkedin size={33} color="black" />
-        </Link>
-        <Link target="_blank" href="mailto:f_fatique@hotmail.com">
-          <CiMail size={35} color="black" />
-        </Link>
-        <Link target="_blank" href="https://github.com/ffatique">
-          <VscGithub size={30} color="black" />
-        </Link>
+        
+        {btwitter == null?
+          <></>
+        :
+          <Link target="_blank" href={btwitter}>
+            <CiTwitter size={35} color="black" />
+          </Link>
+        }
+
+        {byoutube== null?
+          <></>
+        :
+          <Link target="_blank" href={byoutube}>
+            <CiYoutube size={35} color="black" />
+          </Link>
+        }
+
+        {binstagram == null?
+          <></>
+        :
+          <Link target="_blank" href={binstagram}>
+            <CiInstagram size={32} color="black" />
+          </Link>
+        }
+
+        {blinkedin == null?
+          <></>
+        :
+          <Link target="_blank" href={blinkedin}>
+            <CiLinkedin size={33} color="black" />
+          </Link>
+        }
+
+        {bgithub == null?
+          <></>
+        :
+          <Link target="_blank" href={bgithub}>
+            <VscGithub size={33} color="black" />
+          </Link>
+        }
+        
+        {bemail == null?
+          <></>
+        :
+          <Link target="_blank" href={bemail}>
+            <CiMail size={35} color="black" />
+          </Link>
+        }
+        
+        {blens == null?
+          <></>
+        :
+          <Link target="_blank" href={blens}>
+            <Image src={lens} alt="Logo Lens" />
+          </Link>
+        }
+
       </div>
 
       <div className={styles.linkehContainer}>
-        <Link target="_blank" href="https://twitter.com/f_fatique">
+        <Link target="_blank" href="https://www.linkeh.xyz/">
           <Image src={logo} alt="Logo Linkeh" />
         </Link>
       </div>    
     </div>
   )
 }
+
 
