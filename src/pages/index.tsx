9bfab9477159b973776e26759e1fd33929334740
ffatique <f_fatique@hotmail.com>
2023-05-13 10:09:54 -0300

    update final

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,42 +12,25 @@ import { RWebShare } from "react-web-share"
 import { VscGithub } from 'react-icons/vsc'
 
 
-interface Props {
-  bname: string,
-  btitle: string,
-  bens: string,
-  blinkOne: string,
-  blinkTwo: string,
-  blinkThree: string,
-  blinkFour: string,
-  blinkFive: string,
-  btitleOne: string,
-  btitleTwo: string,
-  btitleThree: string,
-  btitleFour: string,
-  btitleFive: string,
-  blens: string,
-  btwitter: string,
-  byoutube: string,
-  binstagram: string,
-  blinkedin: string,
-  bgithub: string,
-  bemail: string,
-  bavatarUrl: string,
-  bavatarOne: string,
-  bavatarTwo: string,
-  bavatarThree: string,
-  bavatarFour: string,
-  bavatarFive: string
-}
+export default function Home(){
 
-export default function Home({ bname, btitle, bens, blinkOne, blinkTwo, blinkThree, blinkFour, blinkFive, btitleOne, btitleTwo, btitleThree, btitleFour,
-  btitleFive, blens, btwitter, byoutube, binstagram, blinkedin, bgithub, bemail, bavatarUrl, bavatarOne, bavatarTwo, bavatarThree, bavatarFour, bavatarFive}: Props){
+  var BlinkOne = "blinkOne"
+  var BlinkTwo = "blinkTwo"
+  var BlinkThree = "blinkThree"
+  var BlinkFour = "blinkFour"
+  var BlinkFive = "blinkFive"
+  var Blens = "blens"
+  var Btwitter = "btwitter"
+  var Byoutube = "byoutube"
+  var Binstagram = "binstagram"
+  var Blinkedin = "blinkedin"
+  var Bgithub = "bgithub"
+  var Bemail = "bemail"
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>Linkeh | {bname}</title>
+        <title>Linkeh | {"bname"}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta content="#515AFF" name="theme-color"></meta>
       </Head>
@@ -56,9 +39,9 @@ export default function Home({ bname, btitle, bens, blinkOne, blinkTwo, blinkThr
         
         <RWebShare
           data={{
-            text: `Linkeh | ${bname}`,
+            text: `Linkeh | ${"bname"}`,
             url: "",
-            title: `Linkeh | ${bname}`,
+            title: `Linkeh | ${"bsecondName"}`,
           }}
           onClick={() => console.log("Shared Successfully!")}
         >
@@ -73,70 +56,70 @@ export default function Home({ bname, btitle, bens, blinkOne, blinkTwo, blinkThr
 
       <main className={styles.mainContainer}>
 
-        <Image src={bavatarUrl} width={120} height={120} alt={`Foto ${bname}`} />
+        <Image src={"bavatarUrl"} width={120} height={120} alt={`Foto ${"bname"}`} />
 
-        <h2>{bname}</h2>
-        <h3>{btitle}</h3>
+        <h2>{"bname"}</h2>
+        <h3>{"bsecondName"}</h3>
         <div className={styles.ensContainer}>
           <Image src={ens} alt="Logo ENS" />
-          <p>{bens}</p>
+          <p>{"bens"}</p>
         </div>
         
-        {blinkOne == null?
+        {BlinkOne == "null"?
           <></>
         : 
           <div className={styles.buttons}>
-            <Link target="_blank" href={blinkOne}>
-              <button><Image src={bavatarOne} width={40} height={40} alt={'Logo Linkeh One'}/>
-                <p>{btitleOne}</p>
+            <Link target="_blank" href={"blinkOne"}>
+              <button><Image src={"bavatarOne"} width={40} height={40} alt={'Logo Linkeh One'}/>
+                <p>{"btitleOne"}</p>
               </button>
             </Link>
           </div>
         }
         
-        {blinkTwo == null?
+        {BlinkTwo == "null"?
           <></>
         : 
           <div className={styles.buttons}>
-            <Link target="_blank" href={blinkTwo}>
-              <button><Image src={bavatarTwo} width={40} height={40} alt={'Logo Linkeh Two'}/>
-                <p>{btitleTwo}</p>
+            <Link target="_blank" href={"blinkTwo"}>
+              <button><Image src={"bavatarTwo"} width={40} height={40} alt={'Logo Linkeh Two'}/>
+                <p>{"btitleTwo"}</p>
               </button>
             </Link>
           </div>
         }
 
-        {blinkThree == null?
+        {BlinkThree == "null"?
           <></>
         : 
           <div className={styles.buttons}>
-            <Link target="_blank" href={blinkThree}>
-              <button><Image src={bavatarThree} width={40} height={40} alt={'Logo Linkeh Three'}/>
-                <p>{btitleThree}</p>
+            <Link target="_blank" href={"blinkThree"}>
+              <button><Image src={"bavatarThree"} width={40} height={40} alt={'Logo Linkeh Three'}/>
+                <p>{"btitleThree"}</p>
               </button>
             </Link>
           </div>
         }
 
-        {blinkFour== null?
+        {BlinkFour == "null"?
           <></>
         : 
           <div className={styles.buttons}>
-            <Link target="_blank" href={blinkFour}>
-              <button><Image src={bavatarFour} width={40} height={40} alt={'Logo Linkeh Four'}/>
-                <p>{btitleFour}</p>
+            <Link target="_blank" href={"blinkFour"}>
+              <button><Image src={"bavatarFour"} width={40} height={40} alt={'Logo Linkeh Four'}/>
+                <p>{"btitleFour"}</p>
               </button>
             </Link>
           </div>
        }
 
-        {blinkFive == null?
+        {BlinkFive == "null"?
           <></>
         : 
           <div className={styles.buttons}>
-            <Link target="_blank" href={blinkFive}>
-              <button><Image src={bavatarFive} width={40} height={40} alt={'Logo Linkeh Five'}/>
-                <p>{btitleFive}</p>
+            <Link target="_blank" href={"blinkFive"}>
+              <button><Image src={"bavatarFive"} width={40} height={40} alt={'Logo Linkeh Five'}/>
+                <p>{"btitleFive"}</p>
               </button>
             </Link>
           </div>
@@ -146,58 +129,58 @@ export default function Home({ bname, btitle, bens, blinkOne, blinkTwo, blinkThr
 
       <div className={styles.footerContainer}>
         
-        {btwitter == null?
+        {Btwitter == "null"?
           <></>
         :
-          <Link target="_blank" href={btwitter}>
+          <Link target="_blank" href={"btwitter"}>
             <CiTwitter size={35} color="black" />
           </Link>
         }
 
-        {byoutube== null?
+        {Byoutube == "null"?
           <></>
         :
-          <Link target="_blank" href={byoutube}>
+          <Link target="_blank" href={"byoutube"}>
             <CiYoutube size={35} color="black" />
           </Link>
         }
 
-        {binstagram == null?
+        {Binstagram == "null"?
           <></>
         :
-          <Link target="_blank" href={binstagram}>
+          <Link target="_blank" href={"binstagram"}>
             <CiInstagram size={32} color="black" />
           </Link>
         }
 
-        {blinkedin == null?
+        {Blinkedin == "null"?
           <></>
         :
-          <Link target="_blank" href={blinkedin}>
+          <Link target="_blank" href={"blinkedin"}>
             <CiLinkedin size={33} color="black" />
           </Link>
         }
 
-        {bgithub == null?
+        {Bgithub == "null"?
           <></>
         :
-          <Link target="_blank" href={bgithub}>
+          <Link target="_blank" href={"bgithub"}>
             <VscGithub size={33} color="black" />
           </Link>
         }
         
-        {bemail == null?
+        {Bemail == "null"?
           <></>
         :
-          <Link target="_blank" href={bemail}>
+          <Link target="_blank" href={"bemail"}>
             <CiMail size={35} color="black" />
           </Link>
         }
         
-        {blens == null?
+        {Blens == "null"?
           <></>
         :
-          <Link target="_blank" href={blens}>
+          <Link target="_blank" href={"blens"}>
             <Image src={lens} alt="Logo Lens" />
           </Link>
         }
@@ -212,5 +195,3 @@ export default function Home({ bname, btitle, bens, blinkOne, blinkTwo, blinkThr
     </div>
   )
 }
-
-
